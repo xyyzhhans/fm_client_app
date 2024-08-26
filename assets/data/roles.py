76ca93e03c685bd5eba1f5489fb17044c84242ec
xyyzhhans <yxygma@gmail.com>
2024-08-26 08:43:50 +0800

    update bwm

--- assets/data/roles.py
+++ assets/data/roles.py
@@ -3,6 +3,11 @@ import json
 # Reading JSON from a file
 with open('roles.json', 'r') as file:
     data = json.load(file)
+    
+for item in data:
+    if item.get('RoleCode') in ['bwmd', 'bwms']:
+        item['Acc'] = item['Pac'] = 2
+        item['Jum'] = 2
 
 # Writing the updated JSON back to the file
 with open('roles.json', 'w') as file:
