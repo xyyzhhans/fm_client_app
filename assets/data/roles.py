021e323e549aa0340282c7bdb3e7fbdc9d142a4b
xyyzhhans <yxygma@gmail.com>
2024-08-26 08:47:18 +0800

    update bpd cbd

--- assets/data/roles.py
+++ assets/data/roles.py
@@ -9,6 +9,10 @@ for item in data:
         item['Acc'] = item['Pac'] = 2
         item['Jum'] = 2
 
+    if item.get('RoleCode') in ['bpdc', 'bpdd', 'bpds', 'cdc', 'cdd', 'cds']:
+        item['Acc'] = item['Pac'] = 2
+        item['Jum'] = 10
+
 # Writing the updated JSON back to the file
 with open('roles.json', 'w') as file:
     json.dump(data, file, indent=4)
