60f71d6a5f1c4d9fa6313c41627041334d49e06a
xyyzhhans <yxygma@gmail.com>
2024-09-20 22:38:34 +0800

    update forward

--- assets/data/roles.py
+++ assets/data/roles.py
@@ -25,6 +25,10 @@ for item in data:
         item['Acc'] = item['Pac'] = 2
         item['Jum'] = 10
         
+    if item.get('RoleCode') in ['afa', 'pfa', 'pfs', 'pfd', 'tfs', 'tfa']:
+        item['Acc'] = item['Pac'] = 10
+        item['Jum'] = 10
+        
 # Writing the updated JSON back to the file
 with open('roles.json', 'w') as file:
     json.dump(data, file, indent=4)
